a6cd0da8239c215e65c41a6e1260c0a1338e8b90
Rainer Sigwald <rainer@sigwald.org>
2018-03-09 22:54:31 -0600

    Minimal INodeLogger impl

--- ReplacingLogger/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ReplacingLogger
-{
-    public class Class1
-    {
-    }
-}
---
+++ ReplacingLogger/ReplacingLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Build.Framework;
+
+namespace ReplacingLogger
+{
+    public class ReplacingLogger : ILogger, INodeLogger
+    {
+        public LoggerVerbosity Verbosity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Parameters { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Initialize(IEventSource eventSource, int nodeCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(IEventSource eventSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
