26c69d8aa461da110ed97e18e4f9323df6d6926a
Rainer Sigwald <rainer@sigwald.org>
2018-03-09 23:43:30 -0600

    Start logger thread

--- ReplacingLogger/ReplacingLogger.cs
+++ ReplacingLogger/ReplacingLogger.cs
@@ -1,26 +1,65 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.Build.Framework;
 
 namespace ReplacingLogger
 {
     public class ReplacingLogger : ILogger, INodeLogger
     {
-        public LoggerVerbosity Verbosity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Parameters { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private static readonly TimeSpan ConsoleRedrawInterval = TimeSpan.FromSeconds(0.25);
+
+        private Thread loggingThread;
+        private CancellationTokenSource CancellationTokenSource;
+        private int NodeCount;
+
+        public LoggerVerbosity Verbosity
+        {
+            get => LoggerVerbosity.Minimal;
+            set { return; }
+        }
+
+        public string Parameters { get; set; }
 
         public void Initialize(IEventSource eventSource, int nodeCount)
         {
-            throw new NotImplementedException();
+            NodeCount = nodeCount;
+            Initialize(eventSource);
         }
 
         public void Initialize(IEventSource eventSource)
         {
-            throw new NotImplementedException();
+            CancellationTokenSource = new CancellationTokenSource();
+
+            loggingThread = new Thread(LoggingThreadProc)
+            {
+                Name = nameof(ReplacingLogger) + " logging thread"
+            };
+            loggingThread.Start(CancellationTokenSource.Token);
         }
 
         public void Shutdown()
         {
-            throw new NotImplementedException();
+            CancellationTokenSource.Cancel();
+
+            Thread.Sleep(ConsoleRedrawInterval);
+
+            Console.WriteLine();
+        }
+
+        private void LoggingThreadProc(object obj)
+        {
+            CancellationToken ct = (CancellationToken)obj;
+
+            int column = Console.CursorLeft;
+            int row = Console.CursorTop;
+
+            System.Console.WriteLine("Starting logger");
+
+            while (!ct.IsCancellationRequested)
+            {
+
+                Thread.Sleep(ConsoleRedrawInterval);
+            }
         }
     }
 }
